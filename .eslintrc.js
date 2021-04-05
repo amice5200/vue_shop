7167877613ab2a447de92816151b1d3274c8eee6
leijinhui <839589736@qq.com>
2021-04-05 23:29:16 +0800

    登录功能完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes': 'off', //双引号
+    'semi': 'off', //末尾分号
+    'comma-dangle': 'off', //逗号
+    "space-before-function-paren": 0
   }
 }
